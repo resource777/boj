6451706317ffeea15924440c3f5ff12ab5472998
resource777 <79133019+resource777@users.noreply.github.com>
2023-01-25 14:18:33 +0900

    [Silver IV] Title: 큐, Time: 208 ms, Memory: 117604 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+n=int(sys.stdin.readline())
+q=deque()
+for _ in range(n):
+    line = sys.stdin.readline().split()
+    if len(line) == 1:
+        if line[0] == 'front':
+            if len(q):  
+                print(q[0])
+            else:
+                print(-1)
+        elif line[0] == 'back':
+            if len(q):  
+                print(q[-1])
+            else:
+                print(-1)
+        elif line[0] == 'size':
+            print(len(q))
+        elif line[0] == 'empty':
+            if len(q):
+                print(0)
+            else:
+                print(1)
+        elif line[0] == 'pop':
+            if len(q):  
+                print(q.popleft())
+            else:
+                print(-1)
+    elif len(line)==2:
+        if line[0]=='push':
+            q.append(int(line[1]))
