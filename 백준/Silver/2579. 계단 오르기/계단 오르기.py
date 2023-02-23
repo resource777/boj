0c14c85be06ccb5bdad25ace125a762108a5f708
resource777 <79133019+resource777@users.noreply.github.com>
2023-02-23 12:40:01 +0900

    [Silver III] Title: 계단 오르기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,11 @@
+n=int(input())
+lst = [int(input()) for _ in range(n)]
+dp = [0]*n
+dp[0]=lst[0]
+if n>1:
+    dp[1]=lst[0]+lst[1]
+if n>2:
+    dp[2]=max(lst[1]+lst[2],lst[0]+lst[2])
+for i in range(3,n):
+    dp[i] = max(dp[i-2]+lst[i],dp[i-3]+lst[i]+lst[i-1])
+print(dp[n-1])
